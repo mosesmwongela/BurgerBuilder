8db54a4674dc32c03d8f9f989e884bc64d5ec9c4
root <root@localhost.localdomain>
2018-05-12 12:41:06 -0400

    Roof

---
+++ burgerbuilder/src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+
+const layout = (props) =>  (
+<Aux>
+    <div> Toolbar, SiderDrawer, Backdrop</div>
+
+    <main>
+        {props.children}
+    </main>
+
+</Aux>
+
+);
+
+export default layout;
