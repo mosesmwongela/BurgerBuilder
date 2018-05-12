b161e0c8737ec492f3917ff147760f055eaae319
root <root@localhost.localdomain>
2018-05-12 14:42:32 -0400

    Loop the ingredients

--- burgerbuilder/src/components/Burger/Burger.js
+++ burgerbuilder/src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    //Object.keys() - extract the keys of a given object and makes an array
+    //map() -> executes a function on each input of the array
+
+
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])]
+        .map((_, i) => {
+           return <BurgerIngredient key = {igKey +1} type={igKey} />
+        });
+    });
     return (
         <div className={classes.Burger}>   
         <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
+       {transformedIngredients}
         <BurgerIngredient type="bread-bottom" />
         </div>
     );
